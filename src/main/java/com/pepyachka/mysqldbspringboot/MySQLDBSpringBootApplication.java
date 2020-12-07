29655ca9b2f636cc96b16d353f3a732417a81921
pepya4ka <ndrozdov9@gmail.com>
2020-12-07 04:30:33 +0300

    spring manage bot

---
+++ src/main/java/com/pepyachka/mysqldbspringboot/MySQLDBSpringBootApplication.java
@@ -0,0 +1,18 @@
+package com.pepyachka.mysqldbspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+
+@SpringBootApplication
+public class MySQLDBSpringBootApplication {
+
+    public static void main(String[] args) {
+
+        ApiContextInitializer.init();
+        SpringApplication.run(MySQLDBSpringBootApplication.class, args);
+
+    }
+
+
+}
---
+++ src/main/java/com/pepyachka/mysqldbspringboot/bot/TelegramBot.java
@@ -0,0 +1,45 @@
+package com.pepyachka.mysqldbspringboot.bot;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private static final String TOKEN = "1428726749:AAF4gt0ptFpMrnCpR53vxsH8CCv6AccWYOE";
+
+    private static final String USERNAME = "PepyachkaCasino_bot";
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()){
+            final long chat_id = update.getMessage().getChatId();
+            sendTextMessage(chat_id, "Привет!");
+        }        
+    }
+
+    private synchronized void sendTextMessage(long chat_id, String message) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(false)
+                .setChatId(chat_id)
+                .setText(message);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+}
