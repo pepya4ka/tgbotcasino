54be56a966ad414affddd5452f1e8f15d4f156a3
Drozdov <Drozdov@lanit.ru>
2021-02-09 17:16:31 +0300

    example commit...

--- src/main/java/com/pepyachka/mysqldbspringboot/bot/TelegramBot.java
+++ src/main/java/com/pepyachka/mysqldbspringboot/bot/TelegramBot.java
@@ -2,13 +2,11 @@ package com.pepyachka.mysqldbspringboot.bot;
 
 import com.pepyachka.mysqldbspringboot.MainController;
 import com.pepyachka.mysqldbspringboot.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.stickers.Sticker;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
