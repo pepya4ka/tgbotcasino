a6b0e8d46a9c2c8a45d301b998f8c3fd3cd01b1d
Drozdov <Drozdov@lanit.ru>
2021-02-27 15:58:34 +0300

    refactor rate<0

--- src/main/java/com/pepyachka/mysqldbspringboot/bot/TelegramBot.java
+++ src/main/java/com/pepyachka/mysqldbspringboot/bot/TelegramBot.java
@@ -48,7 +48,7 @@ public class TelegramBot extends TelegramLongPollingBot {
                     return;
                 }
                 rate = Integer.parseInt(update.getMessage().getText());
-                if (rate > user.getCoins()) {
+                if (rate > user.getCoins() || rate < 0) {
                     rate = 0;
                     sendTextMessage(update.getMessage().getChatId().toString(), "У Вас нет столько монет! \nПожалуйста, введите верное число");
                 }
